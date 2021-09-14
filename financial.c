39e798baabcf4fb66b1572b9049984602e6c95ee
adaahmetyasin <adaahmetyasinedu@yahoo.com>
2021-09-14 20:02:01 +0300

    Add if else statement

--- financial.c
+++ financial.c
@@ -14,16 +14,24 @@ int main(){
     printf("Please enter your currency: ");
     scanf("%s",currency);
     
-    
     do{
         printf("Please enter how many %s how many %s did you buy: \n",currency,coinname);
         scanf("%f %f",&purchasecost,&purchasequantity);
-        
-        totalpurchasecost += purchasecost;
-        totalpurchasequantity += purchasequantity;
 
-        printf("Type yes(y) to enter purchase information, no(n) to enter sales information:\n ");
-        scanf("%s", &choice);
+        if (purchasecost <=0 || purchasequantity <= 0) /* We check if the numbers are positive. */ 
+        {
+            printf("Please enter a valid number!\n");
+            printf("Please enter how many %s how many %s did you buy: \n",currency,coinname);
+            scanf("%f %f",&purchasecost,&purchasequantity);
+        } 
+        else
+        {
+            totalpurchasecost += purchasecost;
+            totalpurchasequantity += purchasequantity;
+
+            printf("Type yes(y) to enter purchase information, no(n) to enter sales information:\n ");
+            scanf("%s", &choice);
+        }
      } while (choice == 'y' || choice == 'Y');
 
 printf("Your total purchase cost is %f\nYour total purchase quantity is %f.\n",totalpurchasecost,totalpurchasequantity);
@@ -32,25 +40,34 @@ printf("Your total purchase cost is %f\nYour total purchase quantity is %f.\n",t
         printf("Please enter how many %s and how many %s you sold:\n ",currency,coinname);
         scanf("%f %f",&salescost,&salesquantity);
         
-        totalsalescost += salescost;
-        totalsalesquantity += salesquantity;
+        if (salescost <= 0 || salesquantity <= 0) /* We check if the numbers are positive. */ 
+        {
+            printf("Please enter a valid number!\n");
+            printf("Please enter how many %s how many %s did you buy: \n",currency,coinname);
+            scanf("%f %f",&salescost,&salesquantity);
+        }
+        else
+        {
+            totalsalescost += salescost;
+            totalsalesquantity += salesquantity;
 
-        printf("Type yes(y) to enter sales information, and no(n) to find out your cost:\n ");
-        scanf("%s", &choice);
+            printf("Type yes(y) to enter sales information, and no(n) to find out your cost:\n ");
+            scanf("%s", &choice);
+        }
      } while (choice == 'y' || choice == 'Y');
 
 printf("Your total sales cost is %f\nYour total sales quantity is %f.\n",totalsalescost,totalsalesquantity);
     
     cost = (totalpurchasecost-totalsalescost) / (totalpurchasequantity-totalsalesquantity);
-    if(cost<0){
+    if(cost<0)
+    {
         printf("You can sell it however you want, you have no cost in this coin :)");
     }
-    else{
-    
-    printf("Your 1 %s' cost is %f %s\n",coinname,cost,currency);
-    printf("I hope you don't lose your money :)");   
+    else
+    {
+        printf("Your 1 %s' cost is %f %s\n",coinname,cost,currency);
+        printf("I hope you don't lose your money :)");   
     }
-    
     puts( "" );  
 
     return 0;
